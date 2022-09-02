d14b4d63962a3f454797fefe1ea7909f7b2df861
cpmart <cpmart@gmail.com>
2022-09-02 16:19:29 -0300

    apagando4

--- ex01.c
+++ ex01.c
@@ -4,6 +4,7 @@ int main(){
 	int numero;
 	scanf("%d",&numero);
 	printf("O valor do número: %d\n", numero);
+	printf("O valor do número: %d\n", numero);
 	printf("O dobro do número: %d\n", 2*numero);	
 	printf("O dobro do número: %d\n", 2*numero);	
 }
