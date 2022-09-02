27c5117d6aff7dae3be0faf6480e80931293c70c
cpmart <cpmart@gmail.com>
2022-09-02 15:49:18 -0300

    atualizando ex01

--- ex01.c
+++ ex01.c
@@ -4,6 +4,5 @@ int main(){
 	int numero;
 	scanf("%d",&numero);
 	printf("O valor do número: %d\n", numero);
-	printf("O dobro do número: %d\n", 2*numero);
-	
+	printf("O dobro do número: %d\n", 2*numero);	
 }
