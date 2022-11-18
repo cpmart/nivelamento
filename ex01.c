22aef2430ea87696569c192622995a233b91288f
cpmart <cpmart@gmail.com>
2022-11-18 11:23:19 -0300

    add linha 7

--- ex01.c
+++ ex01.c
@@ -4,6 +4,7 @@ int main(){
 	int idade;
 	printf("Digite sua idade (18/11/2022): ");
 	printf("\a");		//som
+	printf("\a");		//som
 	scanf("%d",&idade);
 	printf("O dobro da sua idade é: %d anos\n",idade*2);
 }
